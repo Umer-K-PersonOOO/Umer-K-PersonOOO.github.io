a4c7ff6f02bc454a1cdf9f90845729a8080ed7b4
Umer Khan <umerkhan5110@gmail.com>
2023-11-21 11:28:41 -0600

    React-tsx Tailwind setup

---
+++ src/App.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import logo from './logo.svg';
+
+function App() {
+  return (
+    <p className = "text-center text-green-800">
+      Welcome to axon
+    </p>
+  );
+}
+
+export default App;
---
+++ tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{tsx, html, js, jsx}"],
+  // purge: ['./src/**/*.{js,jsx}', './public/index.html'],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
